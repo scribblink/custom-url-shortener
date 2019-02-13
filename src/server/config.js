251b26ec9ee25d0e8c7f4fb2c0b4f4b5d6eaf21a
James Woods <james.woods@aexp.com>
2019-02-13 11:36:51 -0500

    abstract config into a yml file

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,12 @@
-const config = {};
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+let config = {};
+try {
+    config = yaml.safeLoad(fs.readFileSync('/Users/jwood50/Documents/aexp-url-shortener/src/server/config.yml', 'utf8'));
+} catch (e) {
+    console.log(e);
+}
 
 config.redis = {
     host: "127.0.0.1",
@@ -6,3 +14,4 @@ config.redis = {
 }
 
 module.exports = config;
+
--- src/server/lib/redis.js
+++ src/server/lib/redis.js
@@ -1,6 +1,7 @@
 import redis from "redis";
 import config from "../config";
 
-const client = redis.createClient(config.redis);
+const { host, port } = config;
+const client = redis.createClient({host, port});
 
 export default client;
