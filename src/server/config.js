dec38ab421a25cb3daeb557e92c31aaaff78a344
James Woods <james.woods@aexp.com>
2019-02-13 11:40:13 -0500

    update config path

--- src/server/config.js
+++ src/server/config.js
@@ -3,14 +3,9 @@ const fs = require('fs');
 
 let config = {};
 try {
-    config = yaml.safeLoad(fs.readFileSync('/Users/jwood50/Documents/aexp-url-shortener/src/server/config.yml', 'utf8'));
+    config = yaml.safeLoad(fs.readFileSync('src/server/config.yml', 'utf8'));
 } catch (e) {
     console.log(e);
-}
-
-config.redis = {
-    host: "127.0.0.1",
-    port: "6379"
 }
 
 module.exports = config;
